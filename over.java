e8fa732330c6bfadd585bbfea57a036e0776902d
Shafiullah <77819242+shafishafi@users.noreply.github.com>
2022-09-02 20:39:45 +0530

    Create over.java
    sample 1

---
+++ over.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class over
+{	
+static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+         
+        String oldContent = "";
+         
+        BufferedReader reader = null;
+         
+        FileWriter writer = null;
+         
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+             
+            //Reading all the lines of input text file into oldContent
+             
+            String line = reader.readLine();
+             
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                 
+                line = reader.readLine();
+            }
+             
+            //Replacing oldString with newString in the oldContent
+             
+            String newContent = oldContent.replaceAll(oldString, newString);
+             
+            //Rewriting the input text file with newContent
+             
+            writer = new FileWriter(fileToBeModified);
+             
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                //Closing the resources
+                 
+                reader.close();
+                 
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+     
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        System.out.print("Enter FilePath: ");  
+        String str= sc.nextLine(); 
+        System.out.println("Enter SearchPharse: ");
+        String ser= sc.nextLine();             //reads string   
+        modifyFile(str, ser, "####");
+         
+        System.out.println("done");
+    }
+}
